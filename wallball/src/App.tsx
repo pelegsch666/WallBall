6bcdd24f16dd47be5251ec17b2438cf9d824b3e1
pelegsch666 <peleg.mazuz@gmail.com>
2023-07-20 09:15:16 +0300

    ball steady

--- wallball/src/App.tsx
+++ wallball/src/App.tsx
@@ -2,43 +2,53 @@ import React, { useEffect } from 'react';
 import './App.css';
 
 function App() {
-  
-  const [mouseX , setMouseX] = React.useState(0);
-  const [divX , setDivX] = React.useState(0);
-  const [divY , setDivY] = React.useState(0);
+  const [mouseX, setMouseX] = React.useState(0);
+  const [divX, setDivX] = React.useState(0);
+  const [divY, setDivY] = React.useState(0);
+  const [isPlaying, setIsPlaying] = React.useState(false);
 
   useEffect(() => {
-  
     addEventListener('mousemove', e => {
       setDivX(e.clientX);
       setDivY(e.clientY);
-      if (e.clientX > 1200) {
-        setDivX(1200);
-        
+      if (e.clientX > 1430) {
+        setDivX(1430);
       }
-
     });
+  }, [divX, divY]);
 
-  }, [divX]);
+function startGame() { 
+  setIsPlaying(true);
+  const ballElement =  document.querySelector('.ball')
+  
+  ballElement  ? ballElement : '' ;
+}
 
-  function setBallPosition(e: React.MouseEvent<HTMLDivElement>) { 
+
+
+
+  function setBallPosition(e: React.MouseEvent<HTMLDivElement>) {
     console.log(e.target);
   }
 
-const handleMove = (e: React.MouseEvent<HTMLDivElement>) => {
-  console.log(e.clientX);
-}
+  const handleMove = (e: React.MouseEvent<HTMLDivElement>) => {
+    console.log(e.clientX);
+  };
 
   return (
-    <div className="App">
-      <div className="ball" 
-
-      onClick={ e => setBallPosition(e)}
+    <div className="App"
+   
+    >
+      <div
+        className="ball"
+        style={{ left: divX + 10, top: -10 }}
+        onClick={() => startGame()}
       />
       <div
         className="platform"
-        onClick={handleMove}
+        onClick={() => startGame()}
         style={{ left: divX }}
+        
       />
     </div>
   );
